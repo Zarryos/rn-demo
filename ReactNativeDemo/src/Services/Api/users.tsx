f0754eb8ae9f47495c825b8f4cde00fc68ca2da7
Florian Louvet <flouvet@greenbull.group>
2023-02-03 17:18:05 +0100

    add refreshAnUser helper function

--- ReactNativeDemo/src/Services/Api/users.tsx
+++ ReactNativeDemo/src/Services/Api/users.tsx
@@ -1,6 +1,6 @@
 import {baseUrl} from './constants';
 import store from '../../Stores/Redux';
-import {resetUsers} from '../../Stores/Redux/actions';
+import {resetUsers, updateUsers} from '../../Stores/Redux/actions';
 import User from '../../Types/User/User';
 
 /// Create
@@ -10,7 +10,7 @@ import User from '../../Types/User/User';
  * Get users from the placeholder api, reset the users store from the new data
  * @returns Error, if any
  */
-export async function refreshAllUsers(): Promise<String | undefined> {
+export async function refreshAllUsers(): Promise<String | void> {
   const response = await fetch(baseUrl + '/users/');
 
   let error: String | undefined;
@@ -25,6 +25,28 @@ export async function refreshAllUsers(): Promise<String | undefined> {
   if (users?.length) {
     // Note: Only reseting users if we got some, that's a debatable design choise
     store.dispatch(resetUsers.resetUsers(users));
+  }
+}
+
+/**
+ * Get an user from the placeholder api, update this user data in the store
+ * @returns Error, if any
+ */
+export async function refreshAnUser(id: Number): Promise<String | void> {
+  const response = await fetch(baseUrl + '/users/' + id.toString());
+
+  let error: String | undefined;
+  const user: User | undefined = await response.json().catch(e => {
+    error = e;
+  });
+
+  if (error) {
+    return error;
+  }
+
+  if (user) {
+    // Note: Only reseting the user if we got one, that's a debatable design choise
+    store.dispatch(updateUsers.updateUsers([user]));
   }
 }
 
